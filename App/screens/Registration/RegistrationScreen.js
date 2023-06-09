9b4989f4270ade37330efcf0c4982c0a09660410
Luiz-Gusta <luizgustavocorreia2@gmail.com>
2023-06-09 14:08:10 -0300

    savepoint

--- App/screens/Registration/RegistrationScreen.js
+++ App/screens/Registration/RegistrationScreen.js
@@ -1,11 +1,27 @@
-import React, { useContext, useState } from "react";
-import { SafeAreaView } from "react-native";
-
+import React, { useContext, useState, useEffect } from "react";
+import { SafeAreaView, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
 import RegistrationForm from "./RegistrationForm";
+import { BackButtom } from "../../components/BackButton";
 
 export default function RegistrationScreen({ navigation }) {
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity onPress={goBack}>
+          <BackButtom />
+        </TouchableOpacity>
+      ),
+      headerRight: null,
+    });
+
+    function goBack() {
+      navigation.navigate("Login");
+    }
+  }, [navigation]);
+
   return (
     <>
       <SafeAreaView style={styles.topSafeArea} />
